1594b438f895c2d79e48783f24462b1defd0e52d
yuta9999zn <yuta9999k@gmail.com>
2026-02-04 03:17:19 +0700

    Initial commit

---
+++ src/test/java/com/bd/musity/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bd.musity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
